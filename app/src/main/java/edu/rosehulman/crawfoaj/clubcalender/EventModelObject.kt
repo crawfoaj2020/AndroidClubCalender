47182c8efa06d95da6dcb2a743e72b915bef7b88
Alyssa Crawford <crawfoaj@crawfoaj-5510.rose-hulman.edu>
2019-01-11 16:33:50 -0500

    created model object

---
+++ app/src/main/java/edu/rosehulman/crawfoaj/clubcalender/EventModelObject.kt
@@ -0,0 +1,10 @@
+package edu.rosehulman.crawfoaj.clubcalender
+
+data class EventModelObject (
+    var name:String = "",
+    var description:String ="",
+    var location:String = "",
+    var club:String ="",
+    var time:String = "",
+    var date:String = "",
+    var repeatsWeekly:Boolean = true)
